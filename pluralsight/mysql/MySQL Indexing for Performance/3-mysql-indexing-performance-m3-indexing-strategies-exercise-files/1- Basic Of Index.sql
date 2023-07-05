28e8da46aadcdae37bfadce3e0c5b6bddac42bfd
doer <499361328@qq.com>
2023-07-05 18:35:11 +0800

    sql笔记

--- pluralsight/mysql/MySQL Indexing for Performance/3-mysql-indexing-performance-m3-indexing-strategies-exercise-files/1- Basic Of Index.sql
+++ pluralsight/mysql/MySQL Indexing for Performance/3-mysql-indexing-performance-m3-indexing-strategies-exercise-files/1- Basic Of Index.sql
@@ -28,7 +28,13 @@ DROP Index idx_film_length ON film;
 EXPLAIN SELECT first_name, last_name, SUM(amount) AS total
 FROM staff INNER JOIN payment
   ON staff.staff_id = payment.staff_id
+GROUP BY first_name, last_name;
+
+EXPLAIN SELECT first_name, last_name, SUM(amount) AS total
+FROM staff INNER JOIN payment
+  ON staff.staff_id = payment.staff_id
      AND
+     # filtered字段从100->11.11
      payment.payment_date LIKE '2005-08%'
 GROUP BY first_name, last_name;
 
@@ -37,18 +43,21 @@ EXPLAIN FOR CONNECTION 9;
 
 SHOW CHARACTER SET;
 
-SELECT count(*) FROM payment WHERE  payment.payment_date LIKE '2005-08%';
+SELECT count(*) FROM staff;
+SELECT count(picture) FROM staff;
+
+SELECT count(*) FROM payment;
+SELECT count(*) FROM payment WHERE payment.payment_date LIKE '2005-08%';
 SELECT count(*) FROM payment WHERE staff_id=2;
+
 SELECT * FROM INFORMATION_SCHEMA.SCHEMATA;
-SHOW ENGINE INNODB STATUS;
 SELECT NAME, COMMENT FROM INFORMATION_SCHEMA.INNODB_METRICS WHERE NAME LIKE '%ibuf%';
-SELECT (SELECT COUNT(*) FROM INFORMATION_SCHEMA.INNODB_BUFFER_PAGE
-       WHERE PAGE_TYPE LIKE 'IBUF%') AS change_buffer_pages,
+
+SHOW ENGINE INNODB STATUS;
+
+SELECT (SELECT COUNT(*) FROM INFORMATION_SCHEMA.INNODB_BUFFER_PAGE WHERE PAGE_TYPE LIKE 'IBUF%') AS change_buffer_pages,
        (SELECT COUNT(*) FROM INFORMATION_SCHEMA.INNODB_BUFFER_PAGE) AS total_pages,
-       (SELECT ((change_buffer_pages/total_pages)*100))
-       AS change_buffer_page_percentage;
-
-
+       (SELECT ((change_buffer_pages/total_pages)*100)) AS change_buffer_page_percentage;
 
 EXPLAIN ANALYZE
 SELECT first_name, last_name, SUM(amount) AS total
