a6c8930fdc5624223a2c3b3c541748b588ed0dd2
doer <499361328@qq.com>
2023-09-08 23:56:20 +0800

    推荐语文案优化，Pr批量导出视频脚本优化

--- ui/Pr/batch_export_seqs.jsx
+++ ui/Pr/batch_export_seqs.jsx
@@ -4,6 +4,7 @@ var targetSequence = app.project.sequences[0]
 var seqsToDelete = []
 for (var i = 0; i < clips.numItems; i++) {
     var name = clips[i].name
+    $.writeln(name)
     var inPoint = clips[i].inPoint.ticks
     var outPoint = clips[i].outPoint.ticks
     targetSequence.setInPoint(inPoint)
@@ -11,8 +12,8 @@ for (var i = 0; i < clips.numItems; i++) {
     var sequence = targetSequence.createSubsequence(true)
     app.encoder.encodeSequence(
         sequence, 
-        "G:\\沉浸式学习\\ExtendScript\\程序员是怎样创作短视频的\\"+name, 
-        "C:\\Users\\doer\\Documents\\Adobe\\Adobe Media Encoder\\15.0\\Presets\\Copy of Match Source - High bitrate.epr",
+        "G:\\沉浸式学习\\数据结构与算法\\基础\\推荐语\\clips\\"+name, 
+        "C:\\Users\\doer\\Documents\\Adobe\\Adobe Media Encoder\\15.0\\Presets\\My - Match Source - High bitrate.epr",
         0,
         1
     )
