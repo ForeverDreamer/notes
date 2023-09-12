3e515b1b93d15543cc6b37732da038d734c059e3
doer <499361328@qq.com>
2023-09-13 00:39:24 +0800

    Adobe-Cep开发

---
+++ ui/IT.Xuezhang/IT.Xuezhang/host/index.jsx
@@ -0,0 +1,33 @@
+function openDocument(){
+  alert("Hello world!");
+  
+  // var fileRef = new File("~/Downloads/ironman.jpg");
+  // var docRef = app.open(fileRef);
+//   var clips = app.project.sequences[0].videoTracks[0].clips
+// // app.enableQE()
+//   var targetSequence = app.project.sequences[0]
+//   var seqsToDelete = []
+//   for (var i = 0; i < clips.numItems; i++) {
+//       var name = clips[i].name
+//       $.writeln(name)
+//       var inPoint = clips[i].inPoint.ticks
+//       var outPoint = clips[i].outPoint.ticks
+//       targetSequence.setInPoint(inPoint)
+//       targetSequence.setOutPoint(outPoint)
+//       var sequence = targetSequence.createSubsequence(true)
+//       app.encoder.encodeSequence(
+//           sequence, 
+//           "G:\\沉浸式学习\\数据结构与算法\\基础\\推荐语\\clips\\"+name, 
+//           "C:\\Users\\doer\\Documents\\Adobe\\Adobe Media Encoder\\15.0\\Presets\\My - Match Source - High bitrate.epr",
+//           0,
+//           1
+//       )
+//       seqsToDelete.push(sequence)
+//   }
+
+//   app.encoder.startBatch()
+
+//   for (var i = 0; i < seqsToDelete.length; i++) {
+//       app.project.deleteSequence(seqsToDelete[i])
+//   }
+}
